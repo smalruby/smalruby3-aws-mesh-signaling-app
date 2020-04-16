cf333499157f10c1856677f26593ae36a1f6e5a7
Kouji Takao <kouji.takao@gmail.com>
2020-04-17 02:45:37 +0900

    use custom domain.

--- list/app.js
+++ list/app.js
@@ -1,17 +1,22 @@
 const AWS = require('aws-sdk');
 
-const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
+const TTL_SECONDS = 5 * 60;
+const { TABLE_NAME, AWS_REGION } = process.env;
 
-const { TABLE_NAME } = process.env;
+const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: AWS_REGION });
 
 exports.handler = async event => {
     try {
         const connectionId = event.requestContext.connectionId;
         const sourceIp = event.requestContext.identity.sourceIp;
 
+        let domainName = event.requestContext.domainName;
+        if (event.requestContext.apiId) {
+            domainName = `${event.requestContext.apiId}.execute-api.${AWS_REGION}.amazonaws.com`;
+        }
         const apigwManagementApi = new AWS.ApiGatewayManagementApi({
             apiVersion: '2018-11-29',
-            endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
+            endpoint: domainName + '/' + event.requestContext.stage
         });
 
         const response = {
